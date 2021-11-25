d4d6e2079129a93a7a96eb56ef94aaf3f751609c
Jnitz10 <jnitz10@gmail.com>
2021-11-24 21:04:05 -0700

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameBoard = (function() {
+    var board = ["","","","","","","","",""]
+    var symbol = "+"
+    var cells = Array.from(document.querySelectorAll('.cell'));
+    gameBoard.drawBoard = function() {
+        // TODO: get index number for each cell, store as variable
+
+    }
+
+})
+ 
+
+
+
